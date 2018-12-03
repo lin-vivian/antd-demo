e3bc90e3fc850e3fb70c3cce67b81456e6369cc0
linhongjing <linhongjing@weiboyi.com>
2018-12-03 17:18:02 +0800

    test pre-commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Button } from 'antd';
 import './App.less';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className >
+                <Button type="primary">Button</Button>
+            </div>
+        );
+    }
 }
 
 export default App;
